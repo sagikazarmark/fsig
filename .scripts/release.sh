958a2173932277a6a73f49b40c71feebb9da1bda
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-07-12 19:30:42 +0200

    Output instructions from the version script

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -14,8 +14,20 @@ fi
 sed -i -e "s/ENV FSIG_VERSION .*/ENV FSIG_VERSION $VERSION/g" README.md
 rm -f README.md-e
 
+# Update change log with the new version information
 sed -i -e "s/^## \[Unreleased\]$/## [Unreleased]\\"$'\n'"\\"$'\n'"\\"$'\n'"## [${VERSION}] - $(date +%Y-%m-%d)/g" CHANGELOG.md
 rm -f CHANGELOG.md-e
 
 sed -i -e "s|^\[Unreleased\]: \(.*\)HEAD$|[Unreleased]: https://github.com/sagikazarmark/fsig/compare/v${VERSION}...HEAD\\"$'\n'"[${VERSION}]: \1v${VERSION}|g" CHANGELOG.md
 rm -f CHANGELOG.md-e
+
+echo "Version updated to ${VERSION}!
+
+Review the changes made by this script then execute the following:
+
+git add CHANGELOG.md README.md && git tag -S -m 'Prepare release' && git tag -s -m 'Tag v${VERSION}' v${VERSION}
+
+
+Finally, push the changes:
+
+git push; git push --tags"
